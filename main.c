ccce71f47f21d35117de46d5151fd2a6eaa7a10d
Marc Hamamji <hamamjimarc@gmail.com>
2025-09-24 13:49:05 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,39 +1,40 @@
 #include "board.h"
 #include "player.h"
+#include <stdio.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   Player *A = player_new("Name 1");
   Player *B = player_new("Name 2");
-
-  Player players[2] = {A, B};
+  Player *players[2] = {A, B};
 
   Board *board = board_new(players);
+  // check if board is null
 
   board_print(board);
 
-  while (board_state(board) == BOARD_STATE_ONGOING)
-  {
-    Player *current_player = board_get_current_player(board);
+  BoardState state;
 
+  do {
     char column;
-    do
-    {
-      printf("Player xyz enter your move:\n");
-      column = scanf(); // ...
-    } while (board_play(board, column) == 0); // 0 means the move was invalid
+    do {
+      printf("Player %s enter your move: \n",
+             board_get_current_player(board)->name);
+      scanf("%c", &column);
 
+    } while (
+        !board_is_move_valid(board, column)); // 0 means the move was invalid
+    state = board_play(board, column);
     board_print(board);
-  }
+  } while (state == BOARD_STATE_ONGOING);
 
-  BoardState state = board_state(board);
-  if (state == BOARD_STATE_TIE)
-  {
+  if (state == BOARD_STATE_TIE) {
     // print tie
-  }
-  else if (state == BOARD_STATE_WIN)
-  {
-    Player *winner = board_get_winner(board);
+  } else if (state == BOARD_STATE_WIN) {
+    const Player *winner = board_get_winner(board);
     // print winner
   }
+
+  board_free(board);
+  player_free(A);
+  player_free(B);
 }
