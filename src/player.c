b86b26725681ef760ced846249768f7ce560a3e7
Marc Hamamji <hamamjimarc@gmail.com>
2025-10-15 14:28:06 +0300

    Add type to Player, allocate name in heap, add player_is_bot

--- src/player.c
+++ src/player.c
@@ -1,13 +1,22 @@
 #include "player.h"
 #include <stdlib.h>
+#include <string.h>
 
-Player *player_new(char name[], char initial) {
+Player *player_new(char name[], char initial, PlayerType type) {
   Player *player = malloc(sizeof(Player));
 
-  player->name = name;
+  player->name = malloc(strlen(name) + 1);
+  strcpy(player->name, name);
+
   player->initial = initial;
+  player->type = type;
 
   return player;
 }
 
-void player_free(Player *player) { free(player); }
+char player_is_bot(const Player *player) { return player->type != PLAYER_HUMAN; }
+
+void player_free(Player *player) {
+  free(player->name);
+  free(player);
+}
--- src/player.h
+++ src/player.h
@@ -1,10 +1,20 @@
 #pragma once
 
+typedef enum {
+  PLAYER_HUMAN,
+  PLAYER_BOT_EASY,
+  PLAYER_BOT_MODERATE,
+  PLAYER_BOT_HARD,
+} PlayerType;
+
 typedef struct {
   char *name;
   char initial;
+  PlayerType type;
 } Player;
 
-Player *player_new(char name[], char initial);
+Player *player_new(char name[], char initial, PlayerType type);
+
+char player_is_bot(const Player *player);
 
 void player_free(Player *player);
